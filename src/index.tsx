e25acd76130614de2fa63ee9857c02cf077668be
Caio Silva <caio@caiodev.com>
2021-02-10 21:54:48 -0300

    feat(config): eject to add `less-loader`

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,8 @@ import ReactDOM from 'react-dom'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-import 'antd/dist/antd.css'
+import 'antd/lib/style/themes/default.less'
+import 'antd/dist/antd.less'
 import './index.scss'
 
 // https://github.com/ant-design/ant-design/issues/22493
